da607024be785bf8b216ebe96119456df5ffbe56
tonychuangtw <tonychuangtw@gmail.com>
2026-09-20 08:50:20 +0000

    詳解批次（fig）：寫 3 題、跳過 3 題，來自 3 卷
    exp-batch: in=146k out=6k cost=$0.58 turns=9 87s match=^(loc|gao|den|pha|chu|tcm|tea|nur)-

--- js/data/exams.js
+++ js/data/exams.js
@@ -49126,7 +49126,7 @@ window.APP_EXAMS = [
   "subjName": "電子學大意",
   "label": "111 年　初等考試　電子學大意",
   "n": 40,
-  "exp": 18,
+  "exp": 20,
   "mins": 60
  },
  {
@@ -49490,7 +49490,7 @@ window.APP_EXAMS = [
   "subjName": "電子學大意",
   "label": "110 年　初等考試　電子學大意",
   "n": 40,
-  "exp": 15,
+  "exp": 16,
   "mins": 60
  },
  {
