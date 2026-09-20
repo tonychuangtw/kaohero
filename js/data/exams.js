88fb14dc21766d4d7e13d1f93f670c3eea224a5f
tonychuangtw <tonychuangtw@gmail.com>
2026-09-20 10:36:51 +0000

    詳解批次（nofig）：寫 6 題、跳過 34 題，來自 9 卷
    exp-batch: in=113k out=19k cost=$1.01 turns=3 239s match=^(loc|gao|den|pha|chu|tcm|tea|nur)-

--- js/data/exams.js
+++ js/data/exams.js
@@ -47684,7 +47684,7 @@ window.APP_EXAMS = [
   "subjName": "電子學大意",
   "label": "115 年　初等考試　電子學大意",
   "n": 40,
-  "exp": 27,
+  "exp": 29,
   "mins": 60
  },
  {
@@ -47698,7 +47698,7 @@ window.APP_EXAMS = [
   "subjName": "基本電學大意",
   "label": "115 年　初等考試　基本電學大意",
   "n": 40,
-  "exp": 24,
+  "exp": 28,
   "mins": 60
  },
  {
